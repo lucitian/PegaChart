5e8a55aa35a880453182733b4462e50b61ca1da9
lucitian <flippery.slippery28@gmail.com>
2022-04-19 14:56:59 +0800

    Fixed import content

--- django_PegaChart/views.py
+++ django_PegaChart/views.py
@@ -4,19 +4,28 @@ from django.shortcuts import render
 from django import forms
 
 def index(request):
-    ctx = {
-        'pegaID': None,
-        'pegaApiID': None,
-        'checker': None
-    }
-
     if request.method == 'POST':
-        requestPegaID = request.POST['PegaID']
+        ctx = {
+            'pegaID': None,
+            'pegaApiID': None,
+            'checker': None,
+            'pegaApiContent': None
+        }
+        if request.POST.get('PegaID'):
+            requestPegaID = request.POST.get('PegaID')
 
-        ctx['pegaID'] = requestPegaID
-        ctx['pegaApiID'] = f"https://api.pegaxy.io/race/history/pega/{requestPegaID}"
-        ctx['checker'] = 0
+            ctx['pegaID'] = requestPegaID
+            ctx['pegaApiID'] = f"https://api.pegaxy.io/race/history/pega/{requestPegaID}"
+            ctx['checker'] = 0
+
+            return render(request, 'directories/pegachart.html', ctx)
+        elif request.POST.get('PegaApiContent'):
+            requestPegaApiContent = request.POST.get('PegaApiContent')
+
+            ctx['pegaApiContent'] = requestPegaApiContent
+
+            print(ctx['pegaApiContent'])
         
-        return render(request, 'directories/pegachart.html', ctx)
+            return render(request, 'directories/pegachart.html', ctx)
     else:
         return render(request, 'directories/pegachart.html')
