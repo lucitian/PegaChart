714ceebfdedbb4f79a734fb4157babfc19b1ef72
lucitian <flippery.slippery28@gmail.com>
2022-04-19 13:46:35 +0800

    added search ui

--- django_PegaChart/views.py
+++ django_PegaChart/views.py
@@ -4,9 +4,19 @@ from django.shortcuts import render
 from django import forms
 
 def index(request):
+    ctx = {
+        'pegaID': None,
+        'pegaApiID': None,
+        'checker': None
+    }
+
     if request.method == 'POST':
-        pegaID = request.POST['PegaID']
+        requestPegaID = request.POST['PegaID']
 
-        print(f"https://api.pegaxy.io/race/history/pega/{pegaID}")
-
-    return render(request, 'directories/pegachart.html')
+        ctx['pegaID'] = requestPegaID
+        ctx['pegaApiID'] = f"https://api.pegaxy.io/race/history/pega/{requestPegaID}"
+        ctx['checker'] = 0
+        
+        return render(request, 'directories/pegachart.html', ctx)
+    else:
+        return render(request, 'directories/pegachart.html')
